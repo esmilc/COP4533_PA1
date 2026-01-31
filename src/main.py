b530d2d9b9d5c6ed9a63bd69590d44c1a288ca01
esmilc <esmilcanet0711@gmail.com>
2026-01-30 22:19:04 -0500

    Fixed bug in main.py

--- src/main.py
+++ src/main.py
@@ -18,16 +18,16 @@ def main():
         filename = sys.argv[2]
         raw_input = read_file(filename)
         
-        start = time.perf_counter()
+        #start = time.perf_counter()
         output = matching(raw_input)
-        end = time.perf_counter()
-        print("Matching runtime:", end - start, "seconds")
+        #end = time.perf_counter()
+        #print("Matching runtime:", end - start, "seconds")
 
         write_file("output_" + filename, output)
         print("Output written to 'output_" + filename + "'")
 
     elif sys.argv[1] == "verify":
-        if len(sys.argv) <= 4:
+        if len(sys.argv) < 4:
             print("ERROR..missing [preference.txt] and/or [toVerify.txt]")
             return -1
         prefs = sys.argv[2]
@@ -35,10 +35,10 @@ def main():
         raw_input_prefs = read_file(prefs)
         raw_input_verify = read_file(toVerify)
 
-        start = time.perf_counter()
+        #start = time.perf_counter()
         output = verifier(raw_input_prefs, raw_input_verify)
-        end = time.perf_counter()
-        print("Verifier runtime:", end - start, "seconds")
+        #end = time.perf_counter()
+        #print("Verifier runtime:", end - start, "seconds")
         
         print("\n*******")
         print(output)
